20f90b65b0421efb61a1cb4d5800fd43610e9cb0
Michel Oliveira <micheltdoliveira@gmail.com>
2022-05-25 00:50:16 -0300

    add zoom-in and zoom-out

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <math.h>
 
 int X, Y;
+GLfloat eyeX, eyeY, eyeZ;
 
 void init(){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glClearColor(0, 0, 0, 0);  
+
+  eyeX = 0;
+  eyeY = -15;
+  eyeZ = 400;
 
   // enable changing material color
   glEnable (GL_TEXTURE_2D);
@@ -58,7 +63,7 @@ void drawSky(){
     glColor3f(0.0f, 0.0f, 2.0f);
     //glTranslatef(0, -15, 0);
     //glScalef(1.0f,1.0f,1.0f);
-    gluSphere(gluNewQuadric(), 900, 25, 25);
+    gluSphere(gluNewQuadric(), 980, 25, 25);
   glPopMatrix(); 
 
 }
@@ -253,7 +258,8 @@ void draw(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    gluLookAt(0,-80,400, 0,0,0,0,1,0);
+    //gluLookAt(0,-80,400, 0,0,0,0,1,0);
+    gluLookAt(eyeX,eyeY,eyeZ,0,-15,0,0,1,0);
 
 
     glRotatef(X, 1,0,0);
@@ -278,13 +284,25 @@ void specialKeys(int key, int x, int y){
     else if (key == GLUT_KEY_LEFT){
         Y -= 5;
     }
-    else if (key == GLUT_KEY_UP)
+    else if (key == GLUT_KEY_UP){
         X += 5;
-    else if (key == GLUT_KEY_DOWN)
+    }
+    else if (key == GLUT_KEY_DOWN){
         X -= 5;
+    }
+    else if(key == GLUT_KEY_F1){ //zoom-out
+      eyeZ += 5;
+    }
+    else if(key == GLUT_KEY_F2){ //zoom-in
+      if(eyeZ > 5){
+        eyeZ -= 5;
+      }
+    }
+
     //  Request display update
     glutPostRedisplay();
 }
+
 
 int main(int argc, char **argv){
   glutInit(&argc, argv);
